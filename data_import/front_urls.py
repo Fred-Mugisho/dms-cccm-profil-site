9e84311754a7c73d8676dd9868bc5097bd652598
Fred-Mugisho <frederickmugisho228@gmail.com>
2025-08-05 10:27:04 +0200

    Login home pahe ...
    Signed-off-by: Fred-Mugisho <frederickmugisho228@gmail.com>

--- data_import/front_urls.py
+++ data_import/front_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
+    path('', views.login_user, name='login'),
     path('mouvements_deplaces/', views.mouvements_deplaces, name='mouvements_deplaces'),
 ]
