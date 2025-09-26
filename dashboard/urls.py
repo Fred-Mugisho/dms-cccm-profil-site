564b9a8093ad1d63be7ae05a67de1f25f882daf9
Fred-Mugisho <frederickmugisho228@gmail.com>
2025-09-26 14:26:10 +0200

    Correction dashboard, revision profil site, ...
    Signed-off-by: Fred-Mugisho <frederickmugisho228@gmail.com>

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard_v1, name='dashboard'),
     path('refresh_dashboard/', views.refresh_dashboard, name='refresh_dashboard'),
     path('coordinateurs_gestionnaires/', views.coordinateurs_gestionnaires, name='coordinateurs_gestionnaires'),
 ]
